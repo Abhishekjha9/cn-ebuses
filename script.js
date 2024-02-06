30b92775be5dbde6e68761cb56274c39d57f97b6
Abhishek Jha <67514812+Abhishekjha9@users.noreply.github.com>
2024-02-06 13:17:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,44 @@ list.scrollLeft = btn.id ==="left" ? -firstCardWidth: firstCardWidth;
 })
 });
 
+let searchItems = document.getElementById("search-items")
+
+function update(){
+  var selectedCategory = document.getElementById('category-options').value;
+var userName = document.getElementById("name").value
+var desc = document.getElementById("Description").value
+
+var cardDesc = document.getElementById("desc")
+var cardName = document.getElementById("cardname")
+
+
+
+if(selectedCategory == "thanks" || selectedCategory == "new-feature"){
+  var newDiv = document.createElement('div');
+  newDiv.classList.add('card');
+  newDiv.style.height = '300px';
+  // newDiv.style.width = '300px';
+  var existingCardWidth = document.querySelector('.list .card').clientWidth;
+  // Set the width of the new div to match the existing cards
+  newDiv.style.width = existingCardWidth + 'px';
+
+  var image = document.createElement('img');
+  image.src = '666201.png';
+
+  var p1 = document.createElement('p');
+  p1.textContent = userName
+
+  var p2 = document.createElement('p');
+  p2.textContent = desc
+
+  newDiv.appendChild(image);
+  newDiv.appendChild(p1);
+  newDiv.appendChild(p2);
+  newDiv.attributeStyleMap
+
+  document.getElementById('issues').appendChild(newDiv);
+
+  cardDesc.innerText = desc
+  cardName.innerText = userName
+}
+}
